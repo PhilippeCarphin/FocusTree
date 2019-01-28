bf21b447009d415dd8a2229c8c0bb8fedb095115
Philippe Carphin <philippe.carphin@polymtl.ca>
2019-01-27 22:12:14 -0500

    Nide, it prins the tree

--- focus.py
+++ focus.py
@@ -1,4 +1,4 @@
-
+import datetime
 class TreeNode:
     def __init__(self, **kwargs):
         self.children = []
@@ -6,6 +6,8 @@ class TreeNode:
         self.parent = kwargs.get('parent', None)
         self.done = False
         self.update_depth()
+        self.created_on = datetime.datetime.now().strftime("(%Y-%m-%d %H:%M:%S)")
+        self.finished_on = "DOES NOT APPLY"
 
     def update_depth(self):
         self.depth = self.parent.depth + 1 if self.parent else 0
@@ -24,6 +26,15 @@ class TreeNode:
                 return False
 
         return self.done
+
+    def __str__(self):
+        indent = '    ' * self.depth
+        return indent + self.text + f"[created: {self.created_on}, finished: {self.finished_on}]"
+
+    def print_tree(self):
+        print(self)
+        for c in self.children:
+            c.print_tree()
 
 def get_command():
     return input("enter command")
@@ -54,6 +65,7 @@ def run():
             current_task.done = True
             if not current_task.is_done():
                 print("Cannot mark done, task has unfinished children")
+                current_task.done = False
                 for c in current_task.children():
                     if not c.is_done():
                         current_task = c
@@ -66,9 +78,14 @@ def run():
                 if current_task is root_node:
                     current_task = dummy_node
             else:
+                current_task.finished_on = datetime.datetime.now().strftime("(%Y-%m-%d %H:%M:%S)")
                 current_task = current_task.parent
                 if current_task is root_node:
                     current_task = dummy_node
+
+        else:
+            print("UNKNOWN COMMAND " + command)
+            continue
 
 
         if current_task is dummy_node:
@@ -78,7 +95,9 @@ def run():
                     break
             else:
                 print("EVERYTHING IS DONE. GOOD JOB.")
+                root_node.print_tree()
                 quit()
+
 
         print("CURRENT TASK : " + current_task.text)
 if __name__ == "__main__":
