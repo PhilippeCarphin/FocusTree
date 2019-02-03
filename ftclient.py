40970432874f0d66ad212599328336c220e42042
Philippe Carphin <philippe.carphin@polymtl.ca>
2019-02-03 11:36:06 -0500

    Implemented basic request routing based on path

--- ftclient.py
+++ ftclient.py
@@ -1,6 +1,16 @@
 import requests
 
+SERVER_PORT = 8181
+SERVER_ADDRESS = 'localhost'
+
+
 if __name__ == "__main__":
     print("Client Main")
     r = requests.get('http://localhost:8181')
+    print(r.json())
+
+    r = requests.get('http://localhost:8181/fuck_my_face')
+    print(r.json())
+
+    r = requests.get('http://localhost:8181/current_task')
     print(r.json())
--- ftserver.py
+++ ftserver.py
@@ -7,12 +7,29 @@ ADDRESS = '0.0.0.0'
 class FocusTreeRequestHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        print(self)
+        print(self.__dict__)
+        print(self.path)
         self.send_response(200)
-        self.send_header('Content-type', 'text/html')
+        self.send_header('Content-type', 'text/json')
         self.end_headers()
+
+        if self.path == '/fuck_my_face':
+            return self.send_tree()
+        elif self.path == '/current_task':
+            return self.send_current()
+        else:
+            return self.send_tree()
+
+
+    def send_tree(self):
         message = json.dumps({'current_task':"task", "tree":"tree", "ancestors":"ancestors"})
         self.wfile.write(bytes(message, "utf-8"))
+
+    def send_current(self):
+        self.end_headers()
+        message = json.dumps({'current_task': "task"})
+        self.wfile.write(bytes(message, "utf-8"))
+
 
 
 if __name__ == "__main__":
