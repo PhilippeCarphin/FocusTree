9113afe810e951b20493bce3bdc930155dc9fbb4
Philippe Carphin <philippe.carphin@polymtl.ca>
2019-02-07 17:30:37 -0500

    Introduction of basic component
    I had to modify the tslint.json file because I was getting "Invalid source"
    error.  Some googling led me to copy-paste that code and I think the idea is
    that subdirectories of src are not looked at by the transpiler thing unless we
    ask for it.

---
+++ clients/ft-web-client/src/components/demoComponent.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+class DemoComponent extends React.Component {
+    public render() {
+        return <h1>Hello World</h1>;
+    }
+}
+
+export default DemoComponent;
