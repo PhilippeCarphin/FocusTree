2c6681455836ddf3ed163443b367a4256ee8fc28
Philippe Carphin <philippe.carphin@polymtl.ca>
2019-02-07 22:40:38 -0500

    Added stuff to demo component

--- clients/ft-web-client/src/components/demoComponent.tsx
+++ clients/ft-web-client/src/components/demoComponent.tsx
@@ -5,9 +5,30 @@ interface IProps {
     onLaserButtonClick(): any
 }
 
-class DemoComponent extends React.Component<IProps> {
+interface IState {
+    value: string
+}
+
+class DemoComponent extends React.Component<IProps, IState> {
+    constructor(props: any){
+        super(props);
+        this.state = {value: "Enter command"};
+        this.handleFormChange = this.handleFormChange.bind(this);
+        this.handleFormSubmit = this.handleFormSubmit.bind(this);
+    }
     private calculateString(){
         return "a member function returned this";
+    }
+
+    private handleFormSubmit(event: any){
+        console.log("Handling submit of form");
+        console.log(this.state.value);
+        event.preventDefault();
+    }
+
+    private handleFormChange(event: any){
+        this.setState({value: event.target.value});
+        console.log("Handling change of form");
     }
 
     public render() {
@@ -17,6 +38,13 @@ class DemoComponent extends React.Component<IProps> {
                 <p>The value of fmulp is {this.props.fmulp}</p>
                 <p>{this.calculateString()}</p>
                 <button onClick={this.props.onLaserButtonClick}>Activate Lasers</button>
+                <form onSubmit={this.handleFormSubmit}>
+                    <label>
+                        Name:
+                        <input type="text" value={this.state.value} onChange={this.handleFormChange}/>
+                    </label>
+                    <input type="submit" value="submit"/>
+                </form>
             </div>
         );
     }
