917f097cc9a52f3d614d2c593d3016d81dc2fd04
Philippe Carphin <philippe.carphin@polymtl.ca>
2019-02-10 07:04:14 -0500

    Added color to terminal client

--- ftclient.py
+++ ftclient.py
@@ -1,5 +1,6 @@
 import requests
 import focus
+from termcolor import colored
 
 SERVER_PORT = 8181
 SERVER_ADDRESS = 'localhost'
@@ -12,9 +13,13 @@ if __name__ == "__main__":
     print(server_tm.print_tree())
     while True:
 
-        command_line = input('FocusTree> ')
-        r = requests.post('http://localhost:5051/api/send-command', data=command_line)
-        print(r.json()['term_output'])
+        command_line = input(colored('FocusTree> ', 'green'))
+        if command_line == '':
+            continue
+        r = requests.post('http://localhost:5051/api/send-command', data=command_line).json()
+        if r['status'] != 'OK':
+            print(colored('ERROR : ' + r['error'], 'red'))
+        print(r['term_output'])
 
         r = requests.get('http://localhost:5051/api/tree')
         server_tm = focus.TreeManager.from_dict(r.json())
