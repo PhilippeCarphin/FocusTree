1ff55fcbb7534e59d714cfcb7cef9e6224606839
Philippe Carphin <philippe.carphin@polymtl.ca>
2019-02-10 07:04:36 -0500

    Refactored focusnag

--- focusnag.py
+++ focusnag.py
@@ -3,20 +3,21 @@ import requests
 import focus
 import time
 
-if __name__ == "__main__":
+def nag_current_task_ancestors():
     r = requests.get('http://localhost:5051/api/tree')
     server_tm = focus.TreeManager.from_dict(r.json())
-    title = 'FocusTree'
-
-    message = server_tm.current_task.print_ancestors()
 
     print(server_tm.print_tree())
+
+    title = 'FocusTree'
+    message = server_tm.current_task.print_ancestors()
+    subprocess.run([
+        'osascript',
+        '-e',
+        'display alert "' + title + '" message "' + message + '"'
+    ])
+
+if __name__ == "__main__":
     while True:
         time.sleep(30)
-        subprocess.run(
-        [
-	        'osascript', '-e',
-            'display alert "' + title + '" message "' + message + '"'
-        ])
-        r = requests.get('http://localhost:5051/api/tree')
-        server_tm = focus.TreeManager.from_dict(r.json())
+        nag_current_task_ancestors()
