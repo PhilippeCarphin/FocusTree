d7192c9c2efa55331b6353c63bb96fd1efdc31d2
Philippe Carphin <philippe.carphin@polymtl.ca>
2019-02-10 00:26:16 -0500

    Created terminal client

--- ftclient.py
+++ ftclient.py
@@ -1,16 +1,23 @@
 import requests
+import focus
 
 SERVER_PORT = 8181
 SERVER_ADDRESS = 'localhost'
 
 
 if __name__ == "__main__":
-    print("Client Main")
-    r = requests.get('http://localhost:8181')
-    print(r.json())
+    r = requests.get('http://localhost:5051/api/tree')
+    server_tm = focus.TreeManager.from_dict(r.json())
+    server_tm.save_to_file('from_client.json')
+    print(server_tm.print_tree())
+    while True:
+        command_line = input('FocusTree> ')
+        r = requests.post('http://localhost:5051/api/send-command', data=command_line)
 
-    r = requests.get('http://localhost:8181/fuck_my_face')
-    print(r.json())
+        r = requests.get('http://localhost:5051/api/tree')
+        server_tm = focus.TreeManager.from_dict(r.json())
+        server_tm.save_to_file('from_client.json')
+        print(server_tm.print_tree())
 
-    r = requests.get('http://localhost:8181/current_task')
-    print(r.json())
+
+
