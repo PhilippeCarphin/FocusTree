9c9b5e07df0ea2656999bbdd03fa0e118927175b
Philippe Carphin <philippe.carphin@polymtl.ca>
2019-02-11 12:53:17 -0500

    Complete refactor of ftclient
    Note, it now takes a port as a parameter

--- ftclient.py
+++ ftclient.py
@@ -5,26 +5,43 @@ from termcolor import colored
 SERVER_PORT = 8181
 SERVER_ADDRESS = 'localhost'
 
+def read_command():
+    return input(colored('FocusTree> ', 'green'))
+
+def eval_command(command_line):
+    payload = command_line if command_line != '' else 'current'
+    resp = requests.post(
+        'http://{}:{}/api/send-command'.format(SERVER_ADDRESS, PORT_NUMBER),
+        data=payload)
+    return resp.json()
+
+def print_output(resp):
+    if resp['status'] != 'OK':
+        print(colored('ERROR : ' + resp['error'], 'red'))
+    print(resp['term_output'])
+
+def loop():
+    command_line = read_command()
+    resp = eval_command(command_line)
+    if resp:
+        print_output(resp)
+
+def REPL():
+    while True:
+        loop()
+
+def get_tree():
+    resp = requests.get('http://{}:{}/api/tree'.format(SERVER_ADDRESS, PORT_NUMBER))
+    return focus.TreeManager.from_dict(resp.json())
 
 if __name__ == "__main__":
-    r = requests.get('http://localhost:5051/api/tree')
-    server_tm = focus.TreeManager.from_dict(r.json())
-    server_tm.save_to_file('from_client.json')
-    print(server_tm.print_tree())
-    while True:
+    PORT_NUMBER = 5051
+    import sys
+    if len(sys.argv) >= 3:
+        if sys.argv[1] == '--port':
+            PORT_NUMBER = int(sys.argv[2])
 
-        command_line = input(colored('FocusTree> ', 'green'))
-        if command_line == '':
-            continue
-        r = requests.post('http://localhost:5051/api/send-command', data=command_line).json()
-        if r['status'] != 'OK':
-            print(colored('ERROR : ' + r['error'], 'red'))
-        print(r['term_output'])
+    tree = get_tree()
+    print(tree.print_tree())
 
-        r = requests.get('http://localhost:5051/api/tree')
-        server_tm = focus.TreeManager.from_dict(r.json())
-        # server_tm.save_to_file('from_client.json')
-        # print(server_tm.print_tree())
-
-
-
+    REPL()
