23f11c25d2ea9591fac94041043cd50f0c2ab17d
Philippe Carphin <philippe.carphin@polymtl.ca>
2019-02-13 10:38:25 -0500

    Made Terminal client work without changing PWD

--- ftclient.sh
+++ ftclient.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd $(dirname $(python -c "import os,sys; print(os.path.realpath(sys.argv[1]))" ${BASH_SOURCE[0]}))
+ft_dir=$(dirname $(python -c "import os,sys; print(os.path.realpath(sys.argv[1]))" ${BASH_SOURCE[0]}))
 
 my_ip=$(curl ipinfo.io/ip 2>/dev/null)
 if [[ $my_ip == 96.21.220.140 ]] ; then
@@ -11,4 +11,4 @@ fi
 
 port=4051
 echo "Connecting to $apt_rpi on port $port"
-python3 ftclient.py --port $port --host $apt_rpi
+python3 $ft_dir/ftclient.py --port $port --host $apt_rpi
