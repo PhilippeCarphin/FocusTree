d48db3e914a693583959a5394fce790369f3e294
Philippe Carphin <philippe.carphin@polymtl.ca>
2019-02-13 11:29:46 -0500

    Added to-rpi script to build

--- clients/ft-web-client/to-rpi.sh
+++ clients/ft-web-client/to-rpi.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-npm run build
-
 cp ~/Dropbox/Notes/Notes_BUCKET/Notes_FocusTree.html build/help.html
-
 rsync -av ./build/ apt-rpi:~/Documents/GitHub/FocusTree/clients/ft-web-client/build/
