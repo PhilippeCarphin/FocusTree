6ae97b12dd1f9760bb17587b2789e482eff89268
Philippe Carphin <philippe.carphin@polymtl.ca>
2019-02-17 21:13:07 -0500

    Web app launcher adapted for linux

--- ftwebapp.py
+++ ftwebapp.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
 
 from ftclient import get_options
+import sys
 import subprocess
 
 opts = get_options()
 
-command_line = 'open http://{}:{}/index.html'.format(opts.host, opts.port)
+url = 'http://{}:{}/index.html'.format(opts.host, opts.port)
 
-subprocess.run(command_line.split())
+if sys.platform == 'darwin':
+    command = 'open'
+else:
+    command = 'xdg-open'
+
+subprocess.run([command, url])
