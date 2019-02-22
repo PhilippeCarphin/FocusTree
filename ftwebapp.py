47b9a52ec912f077ecb6e03f4cc91b57c2f60092
Philippe Carphin <afsmpca@cmcdev-ws26.cmc.ec.gc.ca>
2019-02-22 16:48:48 -0500

    Added comment

--- ftwebapp.py
+++ ftwebapp.py
@@ -11,6 +11,9 @@ url = 'http://{}:{}/index.html'.format(opts.host, opts.port)
 if sys.platform == 'darwin':
     command = 'open'
 else:
+    # At CMC I have to open this with firefox because the old
+    # version of chrome that I have can't show my web app
+    # TODO Change to xdg-open
     command = 'firefox'
 
 subprocess.Popen([command, url])
