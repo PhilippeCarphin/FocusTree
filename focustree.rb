f711f1a719d90e254544945e8611fb00014a11e9
Philippe Carphin <philippe.carphin@polymtl.ca>
2021-06-10 15:37:23 -0400

    Add homebrew formula

---
+++ focustree.rb
@@ -0,0 +1,37 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Focustree < Formula
+  desc "FocusTree"
+  homepage "https://github.com/philippecarphin/focustree"
+  url "https://github.com/philippecarphin/focustree.git"
+  version "1.0.0"
+  sha256 ""
+  license ""
+
+  depends_on "cmake" => :build
+  depends_on "pandoc" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system "ls"
+    system "pwd"
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build", "--target", "install"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test focustree`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
