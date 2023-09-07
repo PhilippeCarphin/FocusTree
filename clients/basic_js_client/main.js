2b0ad4538f3caafebd3604b9962974178385429e
Philippe Carphin <phil103@hotmail.com>
2023-09-07 12:36:27 -0400

    Display tree as nested <ul>

--- clients/basic_js_client/main.js
+++ clients/basic_js_client/main.js
@@ -11,10 +11,10 @@ let sendCommand = function(){
         if(this.readyState != 4){return;}
         if(this.status != 200){console.log(this); return;}
 
-        responseObject = JSON.parse(this.responseText);
-        console.log(responseObject);
-        console.log("DID I GET HERE");
-        document.getElementById('errors').innerHTML = responseObject.errors;
+        resp = JSON.parse(this.responseText);
+        console.log("resp: ", resp);
+        document.getElementById('errors').innerHTML = resp.error;
+        document.getElementById('output').innerHTML = resp.term_output;
 
         updateTreeView();
         updateCurrentTask();
@@ -33,16 +33,40 @@ let sendCommand = function(){
     console.log(JSON.stringify(obj, null, 2))
     req.send(JSON.stringify(obj, null, 2));
 };
-
+/*
+ * Create a ul whose li are the children of obj recursively
+ */
+function to_ul (obj, currentTaskId) {
+  var i, li, ul = document.createElement ("ul");
+  obj.children.forEach((child) => {
+      console.log(child)
+    li = document.createElement ("li");
+    li.appendChild(document.createTextNode("[" + child.id + "] " + child.text));
+    if(child.id == currentTaskId){
+        li.style.color = "purple"
+        li.style.border = "thin solid purple"
+    } else if(child.info.done){
+        li.style.color = "green"
+    } else {
+        li.style.color = "red"
+    }
+    li.appendChild (to_ul (child, currentTaskId));
+    ul.appendChild (li);
+  })
+  return ul;
+}
 let updateTreeView = function(){
     let req = new XMLHttpRequest();
     req.onreadystatechange = function(){
         if(this.readyState != 4){return;}
         if(this.status != 200){console.log(this); return;}
 
-        document.getElementById("output").innerHTML = this.responseText;
+        const tree = JSON.parse(this.responseText);
+        const tree_div = document.getElementById("tree")
+        tree_div.innerHTML = ""
+        tree_div.appendChild(to_ul({"children": tree.root_nodes}, tree.current_task_id))
     };
-    req.open('GET', '/fuck_my_face');
+    req.open('GET', '/api/tree');
     req.send();
 };
 updateTreeView();
@@ -53,9 +77,8 @@ let updateCurrentTask = function(){
     req.onreadystatechange = function(){
         if(this.readyState != 4){return;}
         if(this.status != 200){console.log(this); return;}
-
-        document.getElementById('current-task').innerHTML = this.responseText;
-
+        const resp = JSON.parse(this.responseText)
+        document.getElementById('current-task').innerHTML = resp.term_output;
     };
     obj = {
         "command": "current",
